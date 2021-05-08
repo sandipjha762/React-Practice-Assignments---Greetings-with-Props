44a0c1e236110554a722aa687835366748522de8
sandipjha762 <sandipjha762@gmail.com>
2021-05-08 16:23:35 +0530

    hey

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,11 +4,11 @@ import React from 'react'
 
 const Welcome=(props)=>{
     return(
-        <div>
+        <details>
         Hey !
         <h1>{props.name}</h1>
         <h2>Welcome to Newton School.</h2>
-        </div>
+        </details>
     )
 
     
